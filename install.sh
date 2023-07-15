fa365c4bb72f35bc305e36c4eddabfe9c2f93296
hypr2771 <vincent.castelluci@gmail.com>
2023-07-15 13:55:35 +0200

    Temp install file (working for my own Raspberry, use similar paths than the ones in `beat-bot.service` for correct result, will adjust later)

---
+++ install.sh
@@ -0,0 +1,10 @@
+mvn clean package
+cd target
+chmod a+x beat-*.jar
+export name=`ls beat-*.jar`
+cd ..
+sed -i "s/beat-1.0-SNAPSHOT.jar/${name}/g" beat-bot.service
+sudo cp beat-bot.service /lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable beat-bot.service
+sudo systemctl restart beat-bot.service
