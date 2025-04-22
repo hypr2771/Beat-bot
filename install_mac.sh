e0159e6d2cc56aa38b19b1a9a0b9d39579318791
hypr2771 <vincent.castelluci@gmail.com>
2025-04-22 16:45:30 +0700

    Handle MacOS

---
+++ install_mac.sh
@@ -0,0 +1,12 @@
+git checkout main
+git pull
+mvn clean package
+cd target
+chmod a+x beat-*.jar
+export name=`ls beat-*.jar`
+cp *.jar ..
+cd ..
+sed -i '' "s/beat-1.0-SNAPSHOT.jar/${name}/g" net.vince.beat-bot.plist
+cp net.vince.beat-bot.plist ~/Library/LaunchAgents/net.vince.beat-bot.plist
+launchctl unload ~/Library/LaunchAgents/net.vince.beat-bot.plist
+launchctl load ~/Library/LaunchAgents/net.vince.beat-bot.plist
